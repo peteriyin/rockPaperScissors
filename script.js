0a94cd3b8d0c8fa1000911d35933e15385117e70
Peter <poderinde619@gmail.com>
2025-11-14 11:53:27 +0100

    logic to play one round

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ let choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     let computerPick = Math.floor(Math.random() * choices.length);
     if (computerPick == 0) {
-        return choices[0]
+        return choices[0];
     } else if (computerPick == 1) {
-        return choices[1]
+        return choices[1];
     }
     return choices[2];
 };
-console.log(getComputerChoice());
+const compChoice = getComputerChoice();
+console.log(compChoice);
+//////////
 
 function getHumanChoice() {
-    let humanPick = prompt('rock, paper or scissors');
-    return humanPick
+    let pick = prompt('rock, paper or scissors');
+    return (pick.toLowerCase());
 };
-console.log(getHumanChoice());
-
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
+const humanChoice = getHumanChoice();
+////////
 
 let humanScore = 0;
 let computerScore = 0;
-function getOutcome(humanChoice, computerChoice) {
+////////
 
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice);
+    if (humanChoice === computerChoice) {
+        return 'Tie'
+    } else if (humanChoice == choices[0] && computerChoice == choices[2] || humanChoice == choices[1] && computerChoice == choices[0] || humanChoice == choices[2] && computerChoice == choices[1]) {
+        return 'You Win!'
+    }
+    return 'Computer Wins!'
 };
+
+console.log(playRound(humanChoice, compChoice));
