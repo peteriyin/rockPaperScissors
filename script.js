e421cefe9acfafde322c0e683b01c1b44ec2b4a0
Peter <poderinde619@gmail.com>
2025-11-17 12:53:35 +0100

    function to play multiple game rounds

--- script.js
+++ script.js
@@ -9,35 +9,47 @@ function getComputerChoice() {
     }
     return choices[2];
 };
-const compChoice = getComputerChoice();
-console.log(compChoice);
 //////////
 
 function getHumanChoice() {
     let pick = prompt('rock, paper or scissors');
-    return (pick.toLowerCase());
+    return pick.toLowerCase();
 };
-const humanChoice = getHumanChoice();
 ////////
 
-let humanScore = 0;
-let computerScore = 0;
+let draw = 'Tie';
+let playerWin = 'You Win!';
+let compWin = 'Computer Wins!';
 ////////
 
-let draw = 'Tie'
-let playerWin = 'You Win!'
-let compWin = 'Computer Wins!'
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice);
-    if (humanChoice === computerChoice) {
-        return draw;
-    } else if (humanChoice == choices[0] && computerChoice == choices[2] || humanChoice == choices[1] && computerChoice == choices[0] || humanChoice == choices[2] && computerChoice == choices[1]) {
-        console.log(`human Score: ${++humanScore}`);
-        return playerWin;
+    function playRound(humanChoice, computerChoice) {
+        console.log(`Human: ${humanChoice} | Bot: ${computerChoice}`);
+        if (humanChoice === computerChoice) {
+            return draw;
+        } else if (humanChoice == choices[0] && computerChoice == choices[2] || humanChoice == choices[1] && computerChoice == choices[0] || humanChoice == choices[2] && computerChoice == choices[1]) {
+            humanScore++
+            console.log(`player score: ${humanScore}`);
+            return playerWin;
+        };
+        computerScore++
+        console.log(`bot score: ${computerScore}`);
+        return compWin;
+    };
+    for (let i = 1; i <= 5; i++) {
+        const playerChoice = getHumanChoice();
+        const compChoice = getComputerChoice();
+        const oneRound = playRound(playerChoice, compChoice);
+        console.log(`Round ${i}: ${oneRound}`);
+    };
+    if (humanScore > computerScore) {
+        return '--Player wins the game--'
+    } else if (humanScore < computerScore) {
+        return '--Bot wins the game--'
     }
-    console.log(`Computer Score: ${++computerScore}`);
-    return compWin;
-};
-
-console.log(playRound(humanChoice, compChoice));
+    return '---TIE!---'
+}
+console.log(playGame());
