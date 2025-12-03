75110867cd0165fc91d65cae2d21d3a77625bf63
Peter <poderinde619@gmail.com>
2025-12-03 14:21:45 +0100

    refactored computer choice

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 'use strict';
 let choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
-    let computerPick = Math.floor(Math.random() * choices.length);
-    if (computerPick == 0) {
-        return choices[0];
-    } else if (computerPick == 1) {
-        return choices[1];
-    }
-    return choices[2];
+    let computerPick = choices[Math.floor(Math.random() * 3)];
+    return computerPick;
 };
 //////////
 
